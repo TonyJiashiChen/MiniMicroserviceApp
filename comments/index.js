45d0cfcc615e0f86b1b015037c8fbcc2934c4d02
TonyJiashiChen <tonychen9661@gmail.com>
2024-01-04 23:53:12 +1100

    handle missing service down case

--- comments/index.js
+++ comments/index.js
@@ -30,15 +30,37 @@ app.post("/posts/:id/comments", async (req, res) => {
       postId: req.params.id,
       id: commentId,
       content,
-      status: 'pending'
+      status: "pending",
     },
   });
 
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("event received:", req.body.type);
+
+  const { type, data } = req.body;
+  if (type === "CommentModerated") {
+    const { id, postId, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/event", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        postId,
+        status,
+        content,
+      },
+    });
+  }
 
   res.send({});
 });
